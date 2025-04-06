493528efed6a76bbaaa622056d8b1fd24ec6264f
Emmanuelhg22 <emmanuel.hernandez5789@alumnos.udg.mx>
2025-04-06 12:15:06 -0600

    [Feat]-> Rediseño de la parte de adicionales y simbologia

--- frontend/src/views/MapaPageView.vue
+++ frontend/src/views/MapaPageView.vue
@@ -7,40 +7,76 @@
     <div class="container">
       <div class="col2" id="mapa"></div>
       <div class="col1" id="simbolos">
-        <div class="simbologia">
-          <ul>
-            <p>Simbología</p>
-            <li class="menu-simbolo">
-              <img src="@/assets/iconos/camion_verde.svg" /><span>Camiones en circulación</span>
-            </li>
-            <li class="menu-simbolo">
-              <img src="@/assets/iconos/parada_camiones.svg" /><span>Paradas de Camiones</span>
-            </li>
-            <li class="menu-simbolo">
-              <img src="@/assets/iconos/Pin_ubicacion.svg" /><span>Tu ubicación</span>
-            </li>
-          </ul>
+        <div class="card" style="width: 18rem;">
+          <div class="adicional card-body">
+            <p class="card-title">Adicional</p>
+            <ul class="card-text">
+              <li class="menu-adicional">
+                <div class="dropdown-center">
+                  <button
+                    class="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Camiones
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Action two</a></li>
+                    <li><a class="dropdown-item" href="#">Action three</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li class="menu-adicional">
+                <div class="dropdown-center">
+                  <button
+                    class="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Rutas
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Action two</a></li>
+                    <li><a class="dropdown-item" href="#">Action three</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-
-        <div class="adicional">
-          <ul>
-            <p>Adicional</p>
-            <li class="menu-adicional">
-              <img src="@/assets/iconos/Fav.svg" /><span>Favoritos</span>
-            </li>
-            <li class="menu-adicional">
-              <img src="@/assets/iconos/parada_camiones.svg" /><span>Paradas cercanas</span>
-            </li>
-            <li class="menu-adicional">
-              <img src="@/assets/iconos/camion_verde.svg" /><span>Camiones cercanos</span>
-            </li>
-            <li class="menu-adicional">
-              <img src="@/assets/iconos/Noti.svg" /><span>Notificaciones</span>
-            </li>
-            <li class="menu-adicional">
-              <img src="@/assets/iconos/Rutas.svg" /><span>Rutas</span>
-            </li>
-          </ul>
+        <div class="card" style="width: 18rem;">
+          <div class="simbologia card-body">
+            <p class="card-title">Simbología</p>
+            <ul class="card-text">
+              <li class="menu-simbolo">
+                <img src="@/assets/iconos/camion_verde.svg" /><span
+                  >Camiones en circulación</span
+                >
+              </li>
+              <li class="menu-simbolo">
+                <img src="@/assets/iconos/parada_camiones.svg" /><span
+                  >Paradas de Camiones</span
+                >
+              </li>
+              <li class="menu-simbolo">
+                <img src="@/assets/iconos/Pin_ubicacion.svg" /><span
+                  >Tu ubicación</span
+                >
+              </li>
+              <li class="menu-simbolo">
+                <img src="@/assets/iconos/coin.svg" /><span>Tarifa</span>
+              </li>
+              <li class="menu-simbolo">
+                <img src="@/assets/iconos/clock-history.svg" /><span
+                  >Horario</span
+                >
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -48,23 +84,23 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-import { crearMapa } from '@/assets/geo/mapa.js'
-import { agregarRutaAlMapa } from '@/assets/geo/ruta_centro_cerrito.js'
-import { agregarMarcadoresAlMapa } from '@/assets/geo/marcadores.js'
+import { onMounted } from "vue";
+import { crearMapa } from "@/assets/geo/mapa.js";
+import { agregarRutaAlMapa } from "@/assets/geo/ruta_centro_cerrito.js";
+import { agregarMarcadoresAlMapa } from "@/assets/geo/marcadores.js";
 
-import AppNavbar from '@/components/AppNavbar.vue'
-import AppSidebar from '@/components/AppSidebar.vue'
-import UserMenu from '@/components/UserMenu.vue'
+import AppNavbar from "@/components/AppNavbar.vue";
+import AppSidebar from "@/components/AppSidebar.vue";
+import UserMenu from "@/components/UserMenu.vue";
 
 onMounted(() => {
-  const container = document.getElementById('mapa')
+  const container = document.getElementById("mapa");
   if (container) {
-    const map = crearMapa(container)
-    agregarRutaAlMapa(map)
-    agregarMarcadoresAlMapa(map)
+    const map = crearMapa(container);
+    agregarRutaAlMapa(map);
+    agregarMarcadoresAlMapa(map);
   } else {
-    console.error('No se encontró el contenedor del mapa')
+    console.error("No se encontró el contenedor del mapa");
   }
-})
+});
 </script>
