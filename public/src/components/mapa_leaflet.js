6a5546156b58644fc98677fffd17bede7421da06
Angela963 <angela.ruvalcaba5887@alumnos.udg.mx>
2025-05-01 11:06:03 -0600

    FIX. Iconos más pequeños

--- public/src/components/mapa_leaflet.js
+++ public/src/components/mapa_leaflet.js
@@ -7,11 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.82569790, -102.79237667],
     [20.82549333, -102.79176333],
     [20.82156000, -102.77916167],
-    [20.82144833, -102.77847167], 
+    [20.82144833, -102.77847167],
     [20.821330, -102.778083], //EMPIEZA LA GLORIETA
     [20.821225, -102.77805667],
     [20.82117167, -102.77803667],
-    [20.82111833, -102.77801167], 
+    [20.82111833, -102.77801167],
     [20.82106667, -102.777975],
     [20.82102, -102.77792],
     [20.82097667, -102.77786167],
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.82112167, -102.77723333],
     [20.82114500, -102.77720333],
     [20.821145, -102.777117], //FIN DE LA GLORIETA
-    [20.820844, -102.775581], 
+    [20.820844, -102.775581],
     [20.81977000, -102.76943000],
     [20.81963167, -102.76909000],
     [20.81960600, -102.76900],
@@ -100,14 +100,14 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.841087, -102.786991],
     [20.841886, -102.786405],
     [20.842046, -102.786286],
-    [20.842160, -102.786195], 
+    [20.842160, -102.786195],
     [20.842315, -102.786054],
     [20.842771, -102.785654],
     [20.843480, -102.784957],
-    [20.844693, -102.783510], 
+    [20.844693, -102.783510],
     [20.846050, -102.781955],
     [20.846040, -102.781969],
-    [20.846388, -102.781714], 
+    [20.846388, -102.781714],
     [20.846742, -102.781328],
     [20.846832, -102.781303],
     [20.846877, -102.781253],
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.841619, -102.790804],
     [20.841803, -102.791459],
     [20.841938, -102.792043],
-    [20.842006, -102.792600], 
+    [20.842006, -102.792600],
     [20.842006, -102.79272667],
     [20.84197667, -102.79272667] //Entrada Uni
   ];
@@ -162,11 +162,11 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.826548, -102.787113],
     [20.824271, -102.787859],
     [20.82156000, -102.77916167],
-    [20.82144833, -102.77847167], 
+    [20.82144833, -102.77847167],
     [20.821330, -102.778083], //EMPIEZA LA GLORIETA
     [20.821225, -102.77805667],
     [20.82117167, -102.77803667],
-    [20.82111833, -102.77801167], 
+    [20.82111833, -102.77801167],
     [20.82106667, -102.777975],
     [20.82102, -102.77792],
     [20.82097667, -102.77786167],
@@ -184,7 +184,7 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.82112167, -102.77723333],
     [20.82114500, -102.77720333],
     [20.821145, -102.777117], //FIN DE LA GLORIETA
-    [20.820844, -102.775581], 
+    [20.820844, -102.775581],
     [20.81977000, -102.76943000],
     [20.81963167, -102.76909000],
     [20.81960600, -102.76900],
@@ -262,14 +262,14 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.841087, -102.786991],
     [20.841886, -102.786405],
     [20.842046, -102.786286],
-    [20.842160, -102.786195], 
+    [20.842160, -102.786195],
     [20.842315, -102.786054],
     [20.842771, -102.785654],
     [20.843480, -102.784957],
-    [20.844693, -102.783510], 
+    [20.844693, -102.783510],
     [20.846050, -102.781955],
     [20.846040, -102.781969],
-    [20.846388, -102.781714], 
+    [20.846388, -102.781714],
     [20.846742, -102.781328],
     [20.846832, -102.781303],
     [20.846877, -102.781253],
@@ -306,7 +306,7 @@ document.addEventListener("DOMContentLoaded", function () {
     [20.841619, -102.790804],
     [20.841803, -102.791459],
     [20.841938, -102.792043],
-    [20.842006, -102.792600], 
+    [20.842006, -102.792600],
     [20.842006, -102.79272667],
     [20.84197667, -102.79272667] //Entrada Uni
   ]
@@ -339,95 +339,103 @@ document.addEventListener("DOMContentLoaded", function () {
   L.marker(rutaC02[0]).addTo(map).bindPopup("Inicio");
   L.marker(rutaC02[rutaC02.length - 1]).addTo(map).bindPopup("Fin");  */
 
-
+  /* MARCADOR AXILIAR PARA IDENTIFICAR UNA COORDENADA
   const coordenada = [20.830667, -102.792525];
   // Crear marcador
   L.marker(coordenada)
     .addTo(map)
     .bindPopup("Cruce San Luis y Carretera Yahualica-Tepa")
     .openPopup();
-    
+  */
 
-    // Lista de coordenadas con sus nombres
-const coordenadasConNombres = [
-  { lat: 20.830667, lon: -102.792525, nombre: "Cruce Carretera Yahualica-Tepa y San Luis" },
-  { lat: 20.825306, lon: -102.791397, nombre: "López Mateos y Cruce Carretera Yahualica-Tepa" },
-  { lat: 20.824266, lon: -102.788171, nombre: "López Mateos" },
-  { lat: 20.822490, lon: -102.782244, nombre: "Cruce López Mateos y Las palmas" },
-  { lat: 20.82092667, lon: -102.77747833, nombre: "Glorieta colonias" },
-  { lat: 20.820770, lon: -102.775401, nombre: "Cruce López Mateos y Mayas" },
-  { lat: 20.820493, lon: -102.773704, nombre: "Calle López Mateos" }, 
-  { lat: 20.820088, lon: -102.771649, nombre: "Cruce López Mateos y Aquiles Sérdan" },
-  { lat: 20.819664, lon: -102.769242, nombre: "Cruce López Mateos y Gómez Morin" },
-  { lat: 20.819704, lon: -102.767261, nombre: "Cruce Félix Ramos y González Gallo"},
-  { lat: 20.818199, lon: -102.766898, nombre: "Sagrada Familia"},
-  { lat: 20.816559, lon: -102.766443, nombre: "Cruce Gonzalez Gallo y J. Cruz Ramirez"},
-  { lat: 20.81579167, lon: -102.76473667, nombre: "Cruce Vallarta y San Martín" },
-  { lat: 20.81417333, lon: -102.76472000, nombre: "Colegio Morelos"},
-  { lat: 20.81290833, lon: -102.76473833, nombre: "Parque del beso"},
-  { lat: 20.80997667, lon: -102.76474833, nombre: "Cruce Niños Heroes y Revolución"},
-  { lat: 20.807910, lon: -102.763795, nombre: "Cruce J.Caro Galindo y Matamoros"},
-  { lat: 20.80665833, lon: -102.76375500, nombre: "Calle Matamoros"},
-  { lat: 20.80137167, lon: -102.76398833, nombre: "Central"},
-  { lat: 20.80467167, lon: -102.75863667, nombre: "Oxxo, Calle Hidalgo"},
-  { lat: 20.80800500, lon: -102.75864167, nombre: "IMSS"},
-  { lat: 20.81015000, lon: -102.75963833, nombre: "Cruce 5 de febrero e Independencia"},
-  { lat: 20.81291000, lon: -102.75977167, nombre: "Cruce Independencia y Colón"},
-  { lat: 20.81544167, lon: -102.75977833, nombre: "Casa de la cultura"},
-  { lat: 20.817432, lon: -102.759674, nombre: "Cruce 16 de septiembre y Moctezuma"},
-  { lat: 20.818954, lon: -102.759874, nombre: "Cruce 16 de septiembre y Mariano J."},
-  { lat: 20.821534, lon: -102.760217, nombre: "Cruce 16 de septiembre y Gral. Anaya"},
-  { lat: 20.82340500, lon: -102.76053167, nombre: "Cruce 16 de sep. y Antonio Rojas"},
-  { lat: 20.82294333, lon: -102.76551667, nombre: "Cruce Antonio Rojas y Alvaro Obregon"},
-  { lat: 20.82093833, lon: -102.76735167, nombre: "Cruce Gral. Anaya y González Gallo"},
-  { lat: 20.82045167, lon: -102.77148500, nombre: "Cruce Gral. Anaya y Aquiles Serdán"},
-  { lat: 20.82043833, lon: -102.77306000, nombre: "Cruce José Gpe y López Mateos"},
-  { lat: 20.82077500, lon: -102.77505667, nombre: "Cruce López Mateos y Mayas"},
-  { lat: 20.82163167, lon: -102.77767000, nombre: "Glorieta Colonias"},
-  { lat: 20.82311000, lon: -102.78362500, nombre: "Cruce Av. López Mateos y Alemania"},
-  { lat: 20.82694833, lon: -102.79018833, nombre: "Cruce Hacienda Mirandilla y Trasquila"},
-  { lat: 20.82548500, lon: -102.79083500, nombre: "Cruce Hacienda Trasquilla y López Mateos"},
-  { lat: 20.83383500, lon: -102.79247500, nombre: "Cruce km 71 y Avila Camacho"},
-  { lat: 20.84169500, lon: -102.79261000, nombre: "Cruce km 71 y Rafael Casillas Aceves"},
-  { lat: 20.847123, lon: -102.781430, nombre: "CUALTOS"},
-  { lat: 20.84104333, lon: -102.79275167},
-  //PARADAS SOLO DE C01
-  { lat: 20.81246667, lon:-102.76562167, nombre: "Cruce Manuel Altamirano y Gpe. Victoria"},
-  { lat: 20.813207, lon: -102.763765, nombre: "Cruce Manuel doblado y matamoros"},
-  { lat: 20.81482500, lon: -102.76377500, nombre: "Cruce Matamoros y Morelos"},
-  { lat: 20.817138, lon: -102.763776, nombre: "Cruce H. Garza y Moctezuma"},
-  { lat: 20.81872833, lon: -102.76695000, nombre: "Gonzalez Gallo"},
-  { lat: 20.81960333, lon:-102.76716000, nombre: "Oxxo Gonzalez Gallo"},
-  { lat: 20.82077167, lon: -102.76749000, nombre: "Escuela 5 de mayo"},
-  { lat: 20.82075667, lon: -102.76915500, nombre: "Cruce Gral Anaya y González hermosillo"},
-  { lat: 20.82048667, lon: -102.77136000, nombre: "Gral Anaya y Aquiles serdan"},
-  { lat: 20.834383, lon: -102.792612},
-  { lat: 20.83232333, lon: -102.79263000},
-  { lat: 20.82685500, lon: -102.79033667, nombre: "Cruce Hda. la Trasquila y Mirandilla"},
-  { lat: 20.827450, lon: -102.790730, nombre: "Cruce Hda. Sta. Ana Apacueco y la Mina"},
-  { lat: 20.827481, lon: -102.787027, nombre: "Cruce Hda. La Mina y de Guadalupe"},
-  { lat: 20.82400500, lon: -102.76564667, nombre: "Cruce Alvaro Obregon y J. Luis Velazco"},
-  { lat: 20.824157, lon: -102.763797, nombre: "Cruce J. Luis Velazco y Allende"},
-  { lat: 20.822373, lon: -102.761678, nombre: "Cruce Madero y Avila Camacho"},
-  { lat: 20.82062000, lon: -102.76152667, nombre: "Cruce Mapelo y Felix Ramos"},
-  { lat: 20.82072167, lon: -102.75806167, nombre: "Cruce Josefa Ortiz y Galeana"},
-  { lat: 20.81696167, lon: -102.75789167, nombre: "Cruce Galeana y Zaragoza"},
-  { lat: 20.81305333, lon: -102.75789167, nombre: "Cruce Pedro Medina y Colón"},
-  { lat: 20.81043500, lon:-102.75777000, nombre: "Cruce Pedro Medina y 5 de Febrero"},
-  { lat: 20.80856833, lon: -102.75929667, nombre: "IMSS familiar"},
-  { lat: 20.80480333, lon: -102.75951500, nombre: "Policlinica"},
-  { lat: 20.80137833, lon: -102.76393833, nombre: "Central"},
-  { lat: 20.80355667, lon: -102.76360000, nombre: "Rio"},
-  { lat: 20.80546333, lon: -102.76363500, nombre: "Unidad deportiva Morelos"},
-  { lat: 20.80985667, lon: -102.76449500, nombre: "Cruce Revolución y Niños Heroes"},
-  { lat: 20.81079167, lon: -102.76567833, nombre: "Cruce Calle y Privada Manuel Altamirano"}
-];
+  // Lista de coordenadas con sus nombres
+  const coordenadasConNombres = [
+    { lat: 20.830667, lon: -102.792525, nombre: "Cruce Carretera Yahualica-Tepa y San Luis" },
+    { lat: 20.825306, lon: -102.791397, nombre: "López Mateos y Cruce Carretera Yahualica-Tepa" },
+    { lat: 20.824266, lon: -102.788171, nombre: "López Mateos" },
+    { lat: 20.822490, lon: -102.782244, nombre: "Cruce López Mateos y Las palmas" },
+    { lat: 20.82092667, lon: -102.77747833, nombre: "Glorieta colonias" },
+    { lat: 20.820770, lon: -102.775401, nombre: "Cruce López Mateos y Mayas" },
+    { lat: 20.820493, lon: -102.773704, nombre: "Calle López Mateos" },
+    { lat: 20.820088, lon: -102.771649, nombre: "Cruce López Mateos y Aquiles Sérdan" },
+    { lat: 20.819664, lon: -102.769242, nombre: "Cruce López Mateos y Gómez Morin" },
+    { lat: 20.819704, lon: -102.767261, nombre: "Cruce Félix Ramos y González Gallo" },
+    { lat: 20.818199, lon: -102.766898, nombre: "Sagrada Familia" },
+    { lat: 20.816559, lon: -102.766443, nombre: "Cruce Gonzalez Gallo y J. Cruz Ramirez" },
+    { lat: 20.81579167, lon: -102.76473667, nombre: "Cruce Vallarta y San Martín" },
+    { lat: 20.81417333, lon: -102.76472000, nombre: "Colegio Morelos" },
+    { lat: 20.81290833, lon: -102.76473833, nombre: "Parque del beso" },
+    { lat: 20.80997667, lon: -102.76474833, nombre: "Cruce Niños Heroes y Revolución" },
+    { lat: 20.807910, lon: -102.763795, nombre: "Cruce J.Caro Galindo y Matamoros" },
+    { lat: 20.80665833, lon: -102.76375500, nombre: "Calle Matamoros" },
+    { lat: 20.80137167, lon: -102.76398833, nombre: "Central" },
+    { lat: 20.80467167, lon: -102.75863667, nombre: "Oxxo, Calle Hidalgo" },
+    { lat: 20.80800500, lon: -102.75864167, nombre: "IMSS" },
+    { lat: 20.81015000, lon: -102.75963833, nombre: "Cruce 5 de febrero e Independencia" },
+    { lat: 20.81291000, lon: -102.75977167, nombre: "Cruce Independencia y Colón" },
+    { lat: 20.81544167, lon: -102.75977833, nombre: "Casa de la cultura" },
+    { lat: 20.817432, lon: -102.759674, nombre: "Cruce 16 de septiembre y Moctezuma" },
+    { lat: 20.818954, lon: -102.759874, nombre: "Cruce 16 de septiembre y Mariano J." },
+    { lat: 20.821534, lon: -102.760217, nombre: "Cruce 16 de septiembre y Gral. Anaya" },
+    { lat: 20.82340500, lon: -102.76053167, nombre: "Cruce 16 de sep. y Antonio Rojas" },
+    { lat: 20.82294333, lon: -102.76551667, nombre: "Cruce Antonio Rojas y Alvaro Obregon" },
+    { lat: 20.82093833, lon: -102.76735167, nombre: "Cruce Gral. Anaya y González Gallo" },
+    { lat: 20.82045167, lon: -102.77148500, nombre: "Cruce Gral. Anaya y Aquiles Serdán" },
+    { lat: 20.82043833, lon: -102.77306000, nombre: "Cruce José Gpe y López Mateos" },
+    { lat: 20.82077500, lon: -102.77505667, nombre: "Cruce López Mateos y Mayas" },
+    { lat: 20.82163167, lon: -102.77767000, nombre: "Glorieta Colonias" },
+    { lat: 20.82311000, lon: -102.78362500, nombre: "Cruce Av. López Mateos y Alemania" },
+    { lat: 20.82694833, lon: -102.79018833, nombre: "Cruce Hacienda Mirandilla y Trasquila" },
+    { lat: 20.82548500, lon: -102.79083500, nombre: "Cruce Hacienda Trasquilla y López Mateos" },
+    { lat: 20.83383500, lon: -102.79247500, nombre: "Cruce km 71 y Avila Camacho" },
+    { lat: 20.84169500, lon: -102.79261000, nombre: "Cruce km 71 y Rafael Casillas Aceves" },
+    { lat: 20.847123, lon: -102.781430, nombre: "CUALTOS" },
+    { lat: 20.84104333, lon: -102.79275167 },
+    //PARADAS SOLO DE C01
+    { lat: 20.81246667, lon: -102.76562167, nombre: "Cruce Manuel Altamirano y Gpe. Victoria" },
+    { lat: 20.813207, lon: -102.763765, nombre: "Cruce Manuel doblado y matamoros" },
+    { lat: 20.81482500, lon: -102.76377500, nombre: "Cruce Matamoros y Morelos" },
+    { lat: 20.817138, lon: -102.763776, nombre: "Cruce H. Garza y Moctezuma" },
+    { lat: 20.81872833, lon: -102.76695000, nombre: "Gonzalez Gallo" },
+    { lat: 20.81960333, lon: -102.76716000, nombre: "Oxxo Gonzalez Gallo" },
+    { lat: 20.82077167, lon: -102.76749000, nombre: "Escuela 5 de mayo" },
+    { lat: 20.82075667, lon: -102.76915500, nombre: "Cruce Gral Anaya y González hermosillo" },
+    { lat: 20.82048667, lon: -102.77136000, nombre: "Gral Anaya y Aquiles serdan" },
+    { lat: 20.834383, lon: -102.792612 },
+    { lat: 20.83232333, lon: -102.79263000 },
+    { lat: 20.82685500, lon: -102.79033667, nombre: "Cruce Hda. la Trasquila y Mirandilla" },
+    { lat: 20.827450, lon: -102.790730, nombre: "Cruce Hda. Sta. Ana Apacueco y la Mina" },
+    { lat: 20.827481, lon: -102.787027, nombre: "Cruce Hda. La Mina y de Guadalupe" },
+    { lat: 20.82400500, lon: -102.76564667, nombre: "Cruce Alvaro Obregon y J. Luis Velazco" },
+    { lat: 20.824157, lon: -102.763797, nombre: "Cruce J. Luis Velazco y Allende" },
+    { lat: 20.822373, lon: -102.761678, nombre: "Cruce Madero y Avila Camacho" },
+    { lat: 20.82062000, lon: -102.76152667, nombre: "Cruce Mapelo y Felix Ramos" },
+    { lat: 20.82072167, lon: -102.75806167, nombre: "Cruce Josefa Ortiz y Galeana" },
+    { lat: 20.81696167, lon: -102.75789167, nombre: "Cruce Galeana y Zaragoza" },
+    { lat: 20.81305333, lon: -102.75789167, nombre: "Cruce Pedro Medina y Colón" },
+    { lat: 20.81043500, lon: -102.75777000, nombre: "Cruce Pedro Medina y 5 de Febrero" },
+    { lat: 20.80856833, lon: -102.75929667, nombre: "IMSS familiar" },
+    { lat: 20.80480333, lon: -102.75951500, nombre: "Policlinica" },
+    { lat: 20.80137833, lon: -102.76393833, nombre: "Central" },
+    { lat: 20.80355667, lon: -102.76360000, nombre: "Rio" },
+    { lat: 20.80546333, lon: -102.76363500, nombre: "Unidad deportiva Morelos" },
+    { lat: 20.80985667, lon: -102.76449500, nombre: "Cruce Revolución y Niños Heroes" },
+    { lat: 20.81079167, lon: -102.76567833, nombre: "Cruce Calle y Privada Manuel Altamirano" }
+  ];
 
-// Marcador para cada coordenada de las paradas
-coordenadasConNombres.forEach(punto => {
-  L.marker([punto.lat, punto.lon])
-    .addTo(map)
-    .bindPopup(punto.nombre);
-});
+  // Crear un ícono más pequeño
+  const iconoMini = L.icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png',
+    iconSize: [10, 16],     // ancho, alto (por defecto es [25, 41], punto medio [15,25])
+    iconAnchor: [5, 16],    // punto del ícono que se alinea con la coordenada
+    popupAnchor: [0, -12]
+  });
 
+  // Aplicar el ícono a cada marcador
+  coordenadasConNombres.forEach(punto => {
+    L.marker([punto.lat, punto.lon], { icon: iconoMini })
+      .addTo(map)
+      .bindPopup(punto.nombre);
+  });
+  
 });
