c8a604c35df9d29f0ed394e9a27c4111a1878471
Emmanuelhg22 <emmanuel.hernandez5789@alumnos.udg.mx>
2025-10-10 22:45:08 -0600

    Refactor HTML structure and styles for camiones and perfil pages; enhance sidebar functionality and add feedback modal
    - Updated HTML structure for camiones.html and perfil.html for better readability and maintainability.
    - Improved sidebar navigation with consistent onclick handlers.
    - Added a feedback modal in perfil.html for user suggestions and reports.
    - Introduced a new CSS file (menu.css) to manage sidebar styles, ensuring it remains fixed and overlays content appropriately.
    - Enhanced sidebar hover effects and adjusted layout for mobile responsiveness.

--- public/src/components/sidebar.js
+++ public/src/components/sidebar.js
@@ -1,19 +1,19 @@
-const aside = document.getElementById('aside'),
-    menu = document.getElementById('menu');
+const aside = document.getElementById("aside"),
+  menu = document.getElementById("menu");
 
 menu.onclick = () => {
-    aside.classList.toggle('active');
+  aside.classList.toggle("active");
 };
 
 function toggleMenu() {
-    var menu = document.getElementById("user-menu");
-    var overlay = document.getElementById("menu-overlay");
+  var menu = document.getElementById("user-menu");
+  var overlay = document.getElementById("menu-overlay");
 
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-        overlay.classList.remove("active");
-    } else {
-        menu.classList.add("active");
-        overlay.classList.add("active");
-    }
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    overlay.classList.remove("active");
+  } else {
+    menu.classList.add("active");
+    overlay.classList.add("active");
+  }
 }
